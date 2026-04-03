1544c0abf00dbb7e0bd8a50805bc1aee5d6163e7
Elina <elinagurbanova1@gmail.com>
2026-04-03 20:48:21 +0200

    js

--- js/task-2.js
+++ js/task-2.js
@@ -24,10 +24,10 @@ const images = [
     alt: 'Lighthouse Coast Sea',
   },
 ];
-const gallery = document.querySelector('.galery');
+const gallery = document.querySelector('.gallery');
 const imagesMarkup = images
   .map(({ url, alt }) => {
-    return `<li class ="gallery-item"><img src ="${url} alt="${alt}" width ="300"> </li>`;
+    return `<li class ="gallery-item"><img src ="${url} alt="${alt}" width = "300"></li>`;
   })
   .join('');
 gallery.insertAdjacentHTML('beforeend', imagesMarkup);
